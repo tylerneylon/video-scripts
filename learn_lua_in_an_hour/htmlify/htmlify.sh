37721a8bb45545da427c564686854602a9ed2188
Tyler Neylon <tylerneylon@gmail.com>
2015-02-11 19:58:47 -0800

    Add next links via htmlify.sh.

--- learn_lua_in_an_hour/htmlify/htmlify.sh
+++ learn_lua_in_an_hour/htmlify/htmlify.sh
@@ -7,7 +7,14 @@ for md_file in ../*.md; do
   title=$(cat $md_file | awk '/^## / { print $0 }' | cut -f4- -d' ')
 
   filename=$(basename $md_file)
+  lastfile=$outfile
   outfile=${filename%%.md}.html
+
+  if [[ ! -z $lastfile ]]; then
+    # Finish up the previous file, including a next link.
+    echo '<a href="'$outfile'" style="float:right">Next</a>' >> $lastfile
+    cat postamble.html >> $lastfile
+  fi
 
   echo htmlifying $filename $title
   echo '  <li><a href="learn_lua_in_an_hour/'$outfile'">'$title'</a></li>' >> contents.html
@@ -18,9 +25,10 @@ for md_file in ../*.md; do
   # Write out the body minus the title.
   cat $md_file | awk '(begin == 1) { print $0 } /^## / { begin = 1 }' | markdown_py >> $outfile
 
-  # Write out the postamble.
-  cat postamble.html   >> $outfile
 done
+
+# The last file still needs a postamble.
+cat postamble.html >> $outfile
 
 echo '</ul>' >> contents.html
 
