9026a371620b13ff49005bc9ae6fff8dabacf8a7
Tyler Neylon <tylerneylon@gmail.com>
2015-02-18 16:44:37 -0800

    Start adding line numbers in part 17.

--- learn_lua_in_an_hour/htmlify/htmlify.sh
+++ learn_lua_in_an_hour/htmlify/htmlify.sh
@@ -2,6 +2,19 @@
 
 echo '<ul>' > contents.html
 
+# This concatenates postamble.html to the end of the filename given as $1.
+# If $2 is present, then it's used to replace the %end_js line in postamble.html;
+# if it's absent, then that line is replaced with a blank line.
+add_postamble() {
+  if [ -z "$2" ]; then
+    # No 2nd arg; replace %end_js with nothing.
+    cat postamble.html | sed 's/%end_js//' >> $1
+  else
+    # Yes 2nd arg; replace %end_js with it.
+    cat postamble.html | awk '/%end_js/ { system("cat '"$2"'"); } !/%end_js/ { print $0; }' >> $1
+  fi
+}
+
 for md_file in ../*.md; do
   # Extract the title.
   title=$(cat $md_file | awk '/^## / { print $0 }' | cut -f4- -d' ')
@@ -10,10 +23,17 @@ for md_file in ../*.md; do
   lastfile=$outfile
   outfile=${filename%%.md}.html
 
-  if [[ ! -z $lastfile ]]; then
+  if [[ ! -z "$lastfile" ]]; then
     # Finish up the previous file, including a next link.
     echo '<a href="'$outfile'" style="float:right">Next</a>' >> $lastfile
-    cat postamble.html >> $lastfile
+
+    # This next bit is very specific for now. If I reuse this script for
+    # other scripts, this would be some good code to generalize.
+    if [ "$lastfile" == "part17.html" ]; then
+      add_postamble "$lastfile" "linenumbers.js"
+    else
+      add_postamble "$lastfile"
+    fi
   fi
 
   echo htmlifying $filename $title
@@ -28,7 +48,7 @@ for md_file in ../*.md; do
 done
 
 # The last file still needs a postamble.
-cat postamble.html >> $outfile
+add_postamble "$outfile"
 
 echo '</ul>' >> contents.html
 
---
+++ learn_lua_in_an_hour/htmlify/linenumbers.js
@@ -0,0 +1,12 @@
+<script>
+  var pre = document.getElementsByTagName('pre');
+  var pl = pre.length;
+  for (var i = 0; i < pl; i++) {
+    pre[i].innerHTML = '<span class="line-number"></span>' + pre[i].innerHTML + '<span class="cl"></span>';
+    var num = pre[i].innerHTML.split(/\n/).length;
+    for (var j = 0; j < num; j++) {
+      var line_num = pre[i].getElementsByTagName('span')[0];
+      line_num.innerHTML += '<span>' + (j + 1) + '</span>';
+    }
+  }
+</script>
